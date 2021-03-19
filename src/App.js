9f147f2f9946d65f0f7bced0737741f87fa92824
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-03-19 23:24:05 +0200

    The FUll Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import Data from "./data";
 
-function App() {
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let Slider = setInterval(() => {
+      IncreamentHandler()
+    }, 5000)
+
+    return () => clearInterval(Slider);
+  })
+
+  const IncreamentHandler = () => {
+    setIndex(prevState => (prevState + 1) % Data.length);
+  }
+  const DecreamentHandler = () => {
+    setIndex(prevState => (prevState - 1 + Data.length) % Data.length);
+  }
+  let Output = [], ClassName = null;
+  Data.map(({ name, image, title, quote }, idx) => {
+    console.log("THe Map Function", idx, index)
+    ClassName = index == idx ? "Active" : (idx === index - 1 || (index === 0 && idx === Data.length - 1)) ? "LastSlide" : "NextSlide";
+
+    return Output.push(
+      <article className={`Content ${ClassName}`}>
+        {/* left Arrow */}
+        <div className="ArrowsContainer">
+          <button onClick={DecreamentHandler}><FiChevronLeft /></button>
+        </div>
+        {/* Content */}
+        <div className="Info">
+          <img src={image} title={name} alt={name} />
+          <h2 className="Name">{name}</h2>
+          <p className="Title">{title}</p>
+          <p className="Quote">{quote}</p>
+          <FaQuoteRight />
+        </div>
+        {/* Right Arrow */}
+        <div className="ArrowsContainer">
+          <button onClick={IncreamentHandler}><FiChevronRight /></button>
+
+        </div>
+      </article>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="MainContainer">
+      <h1 className="MainHeading">Reviews</h1>
+      <section className="SectionCenter">
+        {Output}
+      </section>
+    </main>
+  )
 }
 
 export default App;
